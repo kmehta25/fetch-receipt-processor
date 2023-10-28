5d9ec3d47dccebf5a65e2be69990acf8ab2d3de5
kmehta25 <kmehta25@asu.edu>
2023-10-27 22:02:35 -0500

    re-configuring app @ appropriate directory & migrations

---
+++ fetch_backend/receipt_processor/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Receipt(models.Model):
+    retailer = models.CharField(max_length = 255)
+    purchasDate = models.DateField()
+    purchaseTime = models.TimeField()
+    total = models.DecimalField(max_digits = 6, decimal_places = 2)
+
+class Item(models.Model):
+    receipt = models.ForeignKey(Receipt, related_name = 'items', on_delete = models.CASCADE)
+    shortDescription = models.CharField(max_length = 255)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+
+
